5fceeec4584a703fdcdab3b50de8ac11e977a386
Ren <rjingmembers@gmail.com>
2018-03-12 15:34:54 -0700

    fixed layout issue for experience section

--- src/Views/Experience.js
+++ src/Views/Experience.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import { Segment, Label, Icon, Card, Responsive } from 'semantic-ui-react';
 import ExpSegment from '../Components/expSegment';
 import Exp1 from '../Components/Exp1';
@@ -7,24 +7,34 @@ import Exp3 from '../Components/Exp3';
 import './Views.css';
 
 
-const Experience = () => {
-    return (
-        <div className='bodySection'>
-            <Segment attached='top' className='label-segment'>
-                <Label ribbon color='blue'>Experience</Label>
-            </Segment>
-            <ExpSegment />
-            <Segment attached='bottom'>
-             
-                <Card.Group itemsPerRow={3}>
-                <Exp1 />
-                <Exp2 />
-                <Exp3 />
-                </Card.Group>
-  
-            </Segment>    
-        </div>
-    )
+class Experience extends Component {
+    state = {}
+
+    handleOnUpdate = (e, { width }) => this.setState({ width })
+
+    render(){
+        const { width } = this.state;
+        const itemsPerRow = width >= Responsive.onlyComputer.minWidth ? 3 : 1;
+        return (
+            <div className='bodySection'>
+                <Segment attached='top' className='label-segment'>
+                    <Label ribbon color='blue'>Experience</Label>
+                </Segment>
+                <ExpSegment />
+                <Responsive
+                    as={Segment}
+                    attached='bottom'
+                    fireOnMount
+                    onUpdate={this.handleOnUpdate}> 
+                    <Card.Group itemsPerRow={itemsPerRow}>
+                        <Exp1 />
+                        <Exp2 />
+                        <Exp3 />
+                    </Card.Group>
+                </Responsive>
+            </div>
+        )
+    }
 }
 
 export default Experience;
