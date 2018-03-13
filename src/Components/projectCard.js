b6e4566f3d6066a52dfd48d46aae39acd7e4a37e
Ren <rjingmembers@gmail.com>
2018-03-12 22:03:45 -0700

    refined they styling for github and demo links on project card

--- src/Components/projectCard.js
+++ src/Components/projectCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Icon, Image, Divider, Label } from 'semantic-ui-react';
 
 
 export default class Portfolio extends Component {
@@ -9,11 +9,21 @@ export default class Portfolio extends Component {
 
             <Card color='blue'>
             <Card.Content header={this.props.title} />
-            
                 <Image src={this.props.src}/>
                 <Card.Content extra>
-                    <a href={this.props.git} target="_blank" rel="noopener noreferrer"><Icon name='github'  loading/></a>
-                    <a href={this.props.demo} target="_blank" rel="noopener noreferrer"><Icon name='external'  /></a>
+                <a href={this.props.git} target="_blank" rel="noopener noreferrer">
+                    <Label>
+                        <Icon name='github'  loading/>
+                        GitHub
+                    </Label>
+                </a>
+                <Icon name='plus' />
+                <a href={this.props.demo} target="_blank" rel="noopener noreferrer">
+                    <Label color='green'>
+                        <Icon name='external'  />
+                        Demo
+                    </Label>
+                </a>
                 </Card.Content>
             </Card>
             
