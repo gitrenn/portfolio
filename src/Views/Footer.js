a25720bc087b376a7e634c76837999c0f8a68781
Ren <rjingmembers@gmail.com>
2018-03-13 23:42:26 -0700

    updated styles of footer

--- src/Views/Footer.js
+++ src/Views/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Icon, Container } from 'semantic-ui-react';
+import { Icon, Grid, Segment } from 'semantic-ui-react';
 
 const Footer = () => {
     return (
       <div style={{background: '#2185d0'}}>
-        <Container>
+        <Segment vertical textAlign='center'>   
             <a href='https://www.linkedin.com/in/ren-jing-2372b81b/' target="_blank" rel="noopener noreferrer"><Icon name='linkedin'  size='big' color='grey' inverted/></a>
             <a href='https://github.com/gitrenn' target="_blank" rel="noopener noreferrer"><Icon name='github' size='big' color='grey' inverted loading/></a>
             <a href="mailto:rjingmembers@gmail.com?Subject=Hello%20again" target="_top" rel="noopener noreferrer"><Icon name='mail' size='big' color='grey' inverted /></a>
-        </Container>
+        </Segment>
         </div>
     )
 }
