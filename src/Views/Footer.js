b8bf60caae34b200d4e08842661f5804656d2b99
Ren <rjingmembers@gmail.com>
2018-03-16 18:10:59 -0700

    fixed nested <a> issue for menu component

--- src/Views/Footer.js
+++ src/Views/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
       <div style={{background: '#2185d0'}}>
         <Segment vertical textAlign='center'>   
-            <a href='https://www.linkedin.com/in/ren-jing-2372b81b/' target="_blank" rel="noopener noreferrer"><Icon name='linkedin'  size='big' color='grey' inverted/></a>
-            <a href='https://github.com/gitrenn' target="_blank" rel="noopener noreferrer"><Icon name='github' size='big' color='grey' inverted loading/></a>
-            <a href="mailto:rjingmembers@gmail.com?Subject=Hello%20again" target="_top" rel="noopener noreferrer"><Icon name='mail' size='big' color='grey' inverted /></a>
+            <a href='https://www.linkedin.com/in/ren-jing-2372b81b/' target="_blank" rel="noopener noreferrer"><Icon name='linkedin'  size='huge' color='grey' inverted/></a>
+            <a href='https://github.com/gitrenn' target="_blank" rel="noopener noreferrer"><Icon name='github' size='huge' color='grey' inverted loading/></a>
+            <a href="mailto:rjingmembers@gmail.com?Subject=Hello%20again" target="_top" rel="noopener noreferrer"><Icon name='mail' size='huge' color='grey' inverted /></a>
         </Segment>
         </div>
     )
